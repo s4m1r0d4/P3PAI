120aa9a85cd2813a1ea5ad4252d93e69391ed692
s4m1r0d4 <reddragon987pro@gmail.com>
2023-04-03 11:28:05 -0600

    Added Cryptography Library

---
+++ lib/CryptographyLibrary/Protector.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptographyLibrary;
+
+public class Protector
+{
+    // salt
+    private static readonly byte[] salt = Encoding.Unicode.GetBytes("chinnnngado");
+    private static readonly int iterations = 2000;
+    public static string Encrypt(string plainText, string password)
+    {
+        byte[] encryptedBytes;
+        byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
+
+
+        var aes = Aes.Create();
+        var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations, HashAlgorithmName.SHA256);
+
+        aes.Key = pbkdf2.GetBytes(32); // 256-bit key
+        aes.IV = pbkdf2.GetBytes(16); // 128- bit IV
+
+        // Scoped 'using' directive
+        using MemoryStream ms = new();
+        using CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+
+        cs.Write(plainBytes, 0, plainBytes.Length);
+        encryptedBytes = ms.ToArray();
+
+        return Convert.ToBase64String(encryptedBytes);
+    }
+
+    public static string Decrypt(string cryptoText, string password)
+    {
+        byte[] plainBytes;
+        byte[] cryptoBytes = Convert.FromBase64String(cryptoText);
+
+        var aes = Aes.Create();
+        var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations, HashAlgorithmName.SHA256);
+
+        aes.Key = pbkdf2.GetBytes(32);
+        aes.IV = pbkdf2.GetBytes(16);
+
+        using MemoryStream ms = new();
+        using CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write);
+
+        cs.Write(cryptoBytes, 0, cryptoBytes.Length);
+        plainBytes = ms.ToArray();
+
+        return Encoding.Unicode.GetString(plainBytes);
+    }
+}
