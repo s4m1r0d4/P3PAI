658488ae8ecad016cde2a9027954aa4b05e2e16d
s4m1r0d4 <reddragon987pro@gmail.com>
2023-04-03 12:31:58 -0600

    Added Tests for CryptographyLib and Battled with the demos of rfc2898

--- lib/CryptographyLibrary/Protector.cs
+++ lib/CryptographyLibrary/Protector.cs
@@ -1,9 +1,18 @@
-﻿using System.Security.Cryptography;
+﻿// using System.Security.Cryptography;
+// using System.Text;
+// Si solo incluyes esto de arriba no jala, increible
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
+using System.Xml.Linq;
+using static System.Convert;
 
 namespace CryptographyLibrary;
 
-public class Protector
+public static class Protector
 {
     // salt
     private static readonly byte[] salt = Encoding.Unicode.GetBytes("chinnnngado");
@@ -15,17 +24,26 @@ public class Protector
 
 
         var aes = Aes.Create();
-        var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations, HashAlgorithmName.SHA256);
+        // Ni modo carnal, te quedas desactualizado porque sino me caen los demonios
+        var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
 
         aes.Key = pbkdf2.GetBytes(32); // 256-bit key
         aes.IV = pbkdf2.GetBytes(16); // 128- bit IV
 
-        // Scoped 'using' directive
-        using MemoryStream ms = new();
-        using CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+        /*      // Scoped 'using' directive
+                using MemoryStream ms = new();
+                using CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
 
-        cs.Write(plainBytes, 0, plainBytes.Length);
-        encryptedBytes = ms.ToArray();
+                cs.Write(plainBytes, 0, plainBytes.Length);
+                encryptedBytes = ms.ToArray();
+                // Esta madre está bien curseada */
+
+        using (var ms = new MemoryStream()) {
+            using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write)) {
+                cs.Write(plainBytes, 0, plainBytes.Length);
+            }
+            encryptedBytes = ms.ToArray();
+        }
 
         return Convert.ToBase64String(encryptedBytes);
     }
@@ -36,16 +54,24 @@ public class Protector
         byte[] cryptoBytes = Convert.FromBase64String(cryptoText);
 
         var aes = Aes.Create();
-        var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations, HashAlgorithmName.SHA256);
+        var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
 
         aes.Key = pbkdf2.GetBytes(32);
         aes.IV = pbkdf2.GetBytes(16);
 
-        using MemoryStream ms = new();
+        /* using MemoryStream ms = new();
         using CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write);
 
         cs.Write(cryptoBytes, 0, cryptoBytes.Length);
         plainBytes = ms.ToArray();
+        // Igualmente curseado */
+
+        using (var ms = new MemoryStream()) {
+            using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write)) {
+                cs.Write(cryptoBytes, 0, cryptoBytes.Length);
+            }
+            plainBytes = ms.ToArray();
+        }
 
         return Encoding.Unicode.GetString(plainBytes);
     }
---
+++ tests/CryptographyUnitTests.cs
@@ -0,0 +1,34 @@
+namespace tests;
+using CryptographyLibrary;
+using static System.Console;
+
+public class CryptographyUnitTests
+{
+    [Fact]
+    public void TestEncryptText()
+    {
+        string plainText = "Prueba1";
+        string password = "contrasena";
+        string result = String.Empty;
+
+        result = Protector.Encrypt(plainText, password);
+
+        WriteLine($"'{plainText}' encrypted is '{result}'");
+
+        Assert.True(result.Length > 0, "Failed encrypting");
+    }
+
+    [Fact]
+    public void TestDecryptText()
+    {
+        string encryptedText = "fdtjSsjGbPxf+KluxU1Q2A==";
+        string password = "contrasena";
+        string result = String.Empty;
+
+        result = Protector.Decrypt(encryptedText, password);
+
+        WriteLine($"'{encryptedText}' decrypted is '{result}'");
+
+        Assert.True(result.Length > 0, "Failed decrypting");
+    }
+}
