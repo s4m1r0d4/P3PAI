3d90756a81da96db59b7932b8c445f3a789d63b8
s4m1r0d4 <reddragon987pro@gmail.com>
2023-04-04 00:52:24 -0600

    Begun main program development

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,29 @@
 ﻿namespace src;
 using PeopleLibrary;
 using SerializationLibrary;
+using static System.Console;
 class Program
 {
-    List<Person> people;
-    Serializer serializer;
-    static void Main(string[] args)
+    public static List<Person> people;
+    public static Serializer sc;
+
+    Program()
     {
-        Console.WriteLine("Hello, World!");
+        sc = new(people);
+    }
+
+    public static async Task Main(string[] args)
+    {
+        try {
+            people = await sc.Deserialize();
+        } catch {
+            WriteLine($"Couldn't find local database, project will generate a new one");
+            people = new();
+        }
+
+        if (people.Count != 1)
+            WriteLine($"{people.Count} people have been loaded");
+        else
+            WriteLine($"{people.Count} person has been loaded");
     }
 }
